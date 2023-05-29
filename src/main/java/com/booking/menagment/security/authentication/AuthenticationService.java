565edc566ff5e0f919fe7f113897b557cf00c735
irvintopi <irvintopi@hotmail.com>
2023-05-29 04:37:02 +0200

    Started configuring logging.

--- src/main/java/com/booking/menagment/security/authentication/AuthenticationService.java
+++ src/main/java/com/booking/menagment/security/authentication/AuthenticationService.java
@@ -1,15 +1,16 @@
 package com.booking.menagment.security.authentication;
 
 import com.booking.menagment.model.dto.UserDTO;
-import com.booking.menagment.security.config.TokenService;
 import com.booking.menagment.model.entity.User;
 import com.booking.menagment.repository.UserRepository;
+import com.booking.menagment.security.config.TokenService;
 import com.booking.menagment.validators.UserValidator;
+import jakarta.validation.ValidationException;
 import jakarta.ws.rs.NotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AuthenticationService {
     private final UserValidator userValidator;
     private final TokenService tokenService;
@@ -27,6 +29,8 @@ public class AuthenticationService {
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
         try {
+            log.info("Starting authentication process for email: {}", authenticationRequest.getEmail());
+
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             authenticationRequest.getEmail(),
@@ -35,13 +39,19 @@ public class AuthenticationService {
             );
 
             var user = repository.findByEmail(authenticationRequest.getEmail())
-                    .orElseThrow(() -> new NotFoundException("User not found"));
+                    .orElseThrow(() -> {
+                        log.error("User not found with email: {}", authenticationRequest.getEmail());
+                        return new NotFoundException("User not found");
+                    });
+
+            log.info("Authentication successful for email: {}", authenticationRequest.getEmail());
 
             var jwtToken = tokenService.generateToken(user);
             return AuthenticationResponse.builder()
                     .token(jwtToken)
                     .build();
         } catch (AuthenticationException ex) {
+            log.error("Authentication failed for email: {}", authenticationRequest.getEmail(), ex);
             throw new BadCredentialsException("Invalid email or password", ex);
         }
     }
@@ -50,16 +60,29 @@ public class AuthenticationService {
     public AuthenticationResponse register(RegisterRequest request) {
         UserDTO userDTO = new UserDTO();
         BeanUtils.copyProperties(request, userDTO);
-        userValidator.validateUser(userDTO);
 
-        User user1 = new User();
-        BeanUtils.copyProperties(request, user1);
-        user1.setPassword(passwordEncoder.encode(request.getPassword()));
-        repository.save(user1);
+        try {
+            log.info("Starting user registration for email: {}", request.getEmail());
 
-        var token =  tokenService.generateToken(user1);
-        return AuthenticationResponse.builder()
-                .token(token)
-                .build();
+            userValidator.validateUser(userDTO);
+
+            User user = new User();
+            BeanUtils.copyProperties(request, user);
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+            repository.save(user);
+
+            log.info("User registration successful for email: {}", request.getEmail());
+
+            var token =  tokenService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(token)
+                    .build();
+        } catch (ValidationException ex) {
+            log.error("User registration validation failed for email: {}", request.getEmail(), ex);
+            throw ex;
+        } catch (Exception ex) {
+            log.error("User registration failed for email: {}", request.getEmail(), ex);
+            throw ex;
+        }
     }
 }
